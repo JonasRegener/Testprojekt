02c942c727ba02c0562dd076880971be802d4ee1
JonasRegener <regener.jonas@gmail.comgit config --global user.email>
2021-12-18 03:08:51 +0100

    added Questions and first functions

--- script.js
+++ script.js
@@ -56,10 +56,29 @@ let questions = [
         "right_answer": 1
     },
 ]
+let currentQuestion = 0;
 
 function init() {
-
+    document.getElementById('questions-all').innerHTML = questions.length;
+    showQuestion();
 }
 
+function showQuestion() {
+    let question = questions[currentQuestion];
+    document.getElementById('card-question').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
 
+function answer(selection) {
+    let question = questions[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1);
 
+    console.log('Selected anweser is', selectedQuestionNumber)
+    console.log('Current question is', question)
+    if (selectedQuestionNumber == question['right_answer']) {
+        console.log('richtig')
+    } else { console.log('falsch') }
+} 
